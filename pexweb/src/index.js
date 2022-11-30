5fc8167136983138d0e5eb685f850bef674e480a
bernardo1411 <bernardo1411@hotmail.com>
2022-11-29 23:18:49 -0300

    add font family dmsans

--- pexweb/src/index.js
+++ pexweb/src/index.js
@@ -4,11 +4,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import './assets/fonts/DM_Sans/DMSans-Regular.ttf';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
